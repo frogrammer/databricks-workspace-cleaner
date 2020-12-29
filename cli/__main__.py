8806beda76861f12b2cc55aea8f9157a29bb9c49
Luke Vinton <luvinton@microsoft.com>
2020-12-29 15:17:53 +0000

    handle CLI configuration

--- cli/__main__.py
+++ cli/__main__.py
@@ -2,9 +2,13 @@ from fire import Fire
 from utils import db
 from utils.commands import CommandRegistry
 from commands import *  # noQA
+from databricks_cli.utils import InvalidConfigurationError
 
 def main():
-    Fire(CommandRegistry.commands(), name="dwc")
+    try:
+        Fire(CommandRegistry.commands(), name="dwc")
+    except InvalidConfigurationError:
+        raise 'Please configure databricks CLI using command "databricks configure".'
 
 if __name__ == '__main__':
     main()
