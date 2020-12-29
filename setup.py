e77fac6d29f7cc54d6b871b1145e61297d396b84
Luke Vinton <luvinton@microsoft.com>
2020-12-29 09:26:57 +0000

    init cli scaffold

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setup.py."""
+from setuptools import setup, find_packages
+from cli import __version__
+setup(
+    name='databricks-workspace-cleaner',
+    version=__version__,
+    description='',
+    url='',
+    author='Luke Vinton',
+    author_email='luvinton@microsoft.com',
+    license='NA',
+    packages=find_packages(),
+    install_requires=['fire', 'databricks-cli'],
+    tests_require=[],
+    classifiers=[],
+    test_suite='',
+    entry_points={
+        'console_scripts': [
+            'dwc = __main__:main',
+        ],
+    },
+)
