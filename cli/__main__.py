0f4dc2ca40bcf7f3f17c7cda977febac8322d59a
Luke Vinton <luvinton@microsoft.com>
2020-12-31 12:07:50 +0000

    print help when no args provided

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,15 +1,20 @@
+import argparse
 from fire import Fire
 from utils import db
 from utils.commands import CommandRegistry
 from commands import *  # noQA
 from databricks_cli.utils import InvalidConfigurationError
+import sys
 
 def main():
-    try:
-        Fire(CommandRegistry.commands(), name="dwc")
-    except InvalidConfigurationError:
-        print('Please configure databricks CLI using command "databricks configure --token".')
-        pass
+    if len(sys.argv) == 1:
+        CommandRegistry.print_commands()
+    else:
+        try:
+            Fire(CommandRegistry.commands(), name="dwc")
+        except InvalidConfigurationError:
+            print('Please configure databricks CLI using command "databricks configure --token".')
+            pass
 
 if __name__ == '__main__':
     main()
